0c4e872128b0b34c8293d29072dd6836293618b2
4alf <kingalifa11@gmail.com>
2023-06-21 05:22:59 +0100

    lost track

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import Hero from './components/Hero/Hero';
+import Features from './components/Features/Features';
+import HowItWorks from './components/HowItWorks/HowItWorks';
 
 function App() {
   return (
     <div>
       <div className="container">
         <Navbar/>
-       
+       <Hero/>
+       <Features/>
+       <HowItWorks/>
       </div>
      
     </div>
