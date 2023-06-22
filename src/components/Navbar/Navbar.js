d1dcb3ab213f717f0df018a415de2ab1c72361ed
4alf <kingalifa11@gmail.com>
2023-06-22 01:29:29 +0100

    created new logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
     return (
         <nav>
             <a href="/index.html">
-                <svg width="119" 
-                height="57" 
+                <svg width="100" 
+                height="30" 
                 viewBox="0 0 119 57" 
                 fill="none" 
                 xmlns="http://www.w3.org/2000/svg">
